77cc97cf566c4293a36b61e5ed9e4fa9fdc03d5f
Brendan Donegan brendand <Brendan Donegan brendand@symbian.org>
2009-09-11 14:46:11 +0100

    added send ui dynamic bc tests

---
+++ common/tools/ats/bctest/messagingmw/sendui/sendui.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+# Copyright (c) 2009 Symbian Foundation Ltd
+# This component and the accompanying materials are made available
+# under the terms of the License "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+#
+# Initial Contributors:
+# Symbian Foundation Ltd - initial contribution.
+#
+# Contributors:
+# Brendan Donegan <brendand@symbian.org>
+#
+# Description:
+# Script to build ATS test drop for BC Profiles Engine tests
+
+
+use strict;
+use File::Copy;
+use File::Path;
+
+unlink "sendui.zip";
+##rmtree "temp";
+mkpath "temp/bcsendui/general/testframework";
+mkpath "temp/bcsendui/winscw_udeb";
+
+my $epoc=$ENV{'EPOCROOT'} . "epoc32/";
+copy("sendui.xml",	"temp/test.xml");
+
+copy($epoc . "release/winscw/udeb/senduiapitest.dll",                   "temp/bcsendui/winscw_udeb/senduiapitest.dll");
+copy($epoc . "winscw/c/testframework/testframework_sendui.ini",         "temp/bcsendui/general/testframework/testframework_sendui.ini");
+copy($epoc . "winscw/c/testframework/ui_senduiapitest.cfg",             "temp/bcsendui/general/testframework/ui_senduiapitest.cfg");
+
+system("7z a -tzip sendui.zip ./temp/*");
