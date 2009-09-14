835e83c928e663611cb941826a3da568714b587e
Maciej Seroka <maciejs@symbian.org>
2009-09-14 16:05:18 +0100

    New set added

--- common/tools/ats/bctest/websrv/websrv.pl
+++ common/tools/ats/bctest/websrv/websrv.pl
@@ -10,9 +10,10 @@
 #
 # Contributors:
 # Brendan Donegan <brendand@symbian.org>
+# Maciej Seroka <maciejs@symbian.org>
 #
 # Description:
-# Script to build ATS test drop for BC Profiles Engine tests
+# Script to build ATS test drop for BC Web Services tests
 
 
 use strict;
@@ -29,5 +30,7 @@ copy("websrv.xml",	"temp/test.xml");
 
 copy($epoc . "release/winscw/udeb/CSenServiceConnectionBCTest.dll",     "temp/bcwebsrv/winscw_udeb/CSenServiceConnectionBCTest.dll");
 copy($epoc . "winscw/c/testframework/testframework_connection.ini",     "temp/bcwebsrv/general/testframework/testframework_connection.ini");
+copy($epoc . "release/winscw/udeb/HostletConnectionBCTest.dll",			"temp/bcwebsrv/winscw_udeb/HostletConnectionBCTest.dll");
+copy($epoc . "winscw/c/testframework/TestFramework_hostlet.ini",		"temp/bcwebsrv/general/testframework/TestFramework_hostlet.ini");
 
 system("7z a -tzip websrv.zip ./temp/*");
