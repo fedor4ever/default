65a3b302657733b4762df3073a8e41879a3632a5
Brendan Donegan brendand <Brendan Donegan brendand@symbian.org>
2009-09-16 10:55:26 +0100

    added messaging bc tests

---
+++ common/tools/ats/bctest/messaging/messaging.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# Copyright (c) 2009 Symbian Foundation Ltd
+# This component and the accompanying materials are made available
+# under the terms of the License "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+#
+# Initial Contributors:
+# Symbian Foundation Ltd - initial contribution.
+#
+# Contributors:
+# Brendan Donegan <brendand@symbian.org>
+#
+# Description:
+# Script to build ATS test drop for BC Web Services tests
+
+
+use strict;
+use File::Copy;
+use File::Path;
+
+unlink "messaging.zip";
+##rmtree "temp";
+mkpath "temp/bcmessaging/testframework/general";
+mkpath "temp/bcmessaging/resource/general";
+mkpath "temp/bcmessaging/winscw_udeb";
+
+my $epoc=$ENV{'EPOCROOT'} . "epoc32/";
+copy("messaging.xml",       "temp/test.xml");
+
+copy($epoc . "release/winscw/udeb/MMSClientMTMBCTester.dll",                "temp/bcmessaging/winscw_udeb/MMSClientMTMBCTester.dll");
+copy($epoc . "winscw/c/testframework/MMSClientMTMBCTester.ini",             "temp/bcmessaging/testframework/general/MMSClientMTMBCTester.ini");
+copy($epoc . "winscw/c/testframework/MMSClientMTMBCTester_TestImage.jpg",   "temp/bcmessaging/testframework/general/MMSClientMTMBCTester_TestImage.jpg");
+copy($epoc . "release/winscw/udeb/PluginBioControlAPITest.dll",             "temp/bcmessaging/winscw_udeb/PluginBioControlAPITest.dll");
+copy($epoc . "winscw/c/testframework/tcTestUIBioCombiner.cfg",              "temp/bcmessaging/testframework/general/tcTestUIBioCombiner.cfg");
+copy($epoc . "winscw/c/testframework/ui_PluginBioControlAPITest.cfg",       "temp/bcmessaging/testframework/general/ui_PluginBioControlAPITest.cfg");
+copy($epoc . "winscw/c/testframework/test.txt",                             "temp/bcmessaging/testframework/general/test.txt");
+copy($epoc . "winscw/c/testframework/PluginBioControlAPITest.rsc",          "temp/bcmessaging/testframework/general/PluginBioControlAPITest.rsc");
+copy($epoc . "data/z/resource/pluginbiocontrolapitest.rsc",                 "temp/bcmessaging/resource/general/pluginbiocontrolapitest.rsc");
+copy($epoc . "data/z/resource/pluginbiocontrolapitest.r01",                 "temp/bcmessaging/resource/general/pluginbiocontrolapitest.r01");
+
+system("7z a -tzip messaging.zip ./temp/*");
