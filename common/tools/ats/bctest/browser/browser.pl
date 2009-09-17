42bdf207b235f520958da78379d937ddd22faf49
Louis Henry Nayegon <louisn@symbian.org>
2009-09-17 11:20:34 +0100

    Add http download manager tests

--- common/tools/ats/bctest/browser/browser.pl
+++ common/tools/ats/bctest/browser/browser.pl
@@ -22,10 +22,14 @@ use File::Path;
 unlink "browser.zip";
 ##rmtree "temp";
 mkpath "temp/bcbrowser/winscw_udeb";
+mkpath "temp/bcbrowser/general/testframework";
 
 my $epoc=$ENV{'EPOCROOT'} . "epoc32/";
 copy("browser.xml", "temp/test.xml");
 
-copy($epoc . "release/winscw/udeb/favouritesbctest.dll",     "temp/bcbrowser/winscw_udeb/favouritesbctest.dll");
+copy($epoc . "release/winscw/udeb/favouritesbctest.dll",		"temp/bcbrowser/winscw_udeb/favouritesbctest.dll");
+copy($epoc . "release/winscw/udeb/downloadmgrbctest.dll",		"temp/bcbrowser/winscw_udeb/downloadmgrbctest.dll");
+
+copy($epoc . "winscw/c/testframework/testframework_dwmgr.ini",	"temp/bcbrowser/general/testframework/testframework_dwmgr.ini");
 
 system("7z a -tzip browser.zip ./temp/*");
