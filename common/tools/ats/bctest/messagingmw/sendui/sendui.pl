383f90e36277fa53685483209e68ce7efc980bae
brendand <devnull@localhost>
2009-10-09 15:14:37 +0100

    sorted out more stuff with the dynamic bc, fewer failures now

--- common/tools/ats/bctest/messagingmw/sendui/sendui.pl
+++ common/tools/ats/bctest/messagingmw/sendui/sendui.pl
@@ -12,7 +12,7 @@
 # Brendan Donegan <brendand@symbian.org>
 #
 # Description:
-# Script to build ATS test drop for BC Profiles Engine tests
+# Script to build ATS test drop for BC Send UI tests
 
 
 use strict;
@@ -20,15 +20,17 @@ use File::Copy;
 use File::Path;
 
 unlink "sendui.zip";
-##rmtree "temp";
+rmtree "temp";
 mkpath "temp/bcsendui/general/testframework";
+mkpath "temp/bcsendui/general/senduiapitest";
 mkpath "temp/bcsendui/winscw_udeb";
 
 my $epoc=$ENV{'EPOCROOT'} . "epoc32/";
 copy("sendui.xml",	"temp/test.xml");
 
-copy($epoc . "release/winscw/udeb/senduiapitest.dll",                   "temp/bcsendui/winscw_udeb/senduiapitest.dll");
-copy($epoc . "winscw/c/testframework/testframework_sendui.ini",         "temp/bcsendui/general/testframework/testframework_sendui.ini");
-copy($epoc . "winscw/c/testframework/ui_senduiapitest.cfg",             "temp/bcsendui/general/testframework/ui_senduiapitest.cfg");
+copy($epoc . "release/winscw/udeb/senduiapitest.dll",                   "temp/bcsendui/winscw_udeb/senduiapitest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/testframework_sendui.ini",         "temp/bcsendui/general/testframework/testframework_sendui.ini") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/ui_senduiapitest.cfg",             "temp/bcsendui/general/testframework/ui_senduiapitest.cfg") or die "failed : $!";
+copy($epoc . "winscw/c/data/testdata/senduiapitest/attachment.txt",		"temp/bcsendui/general/senduiapitest/attachment.txt") or die "failed : $!";
 
 system("7z a -tzip sendui.zip ./temp/*");
