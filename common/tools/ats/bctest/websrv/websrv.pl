5ab641cda51955b2447b8fdf4d19551eb7a9dd89
brendand <devnull@localhost>
2009-10-20 15:45:42 +0100

    Added all of the tests for ASP

--- common/tools/ats/bctest/websrv/websrv.pl
+++ common/tools/ats/bctest/websrv/websrv.pl
@@ -21,18 +21,26 @@ use File::Copy;
 use File::Path;
 
 unlink "websrv.zip";
-##rmtree "temp";
+rmtree "temp";
 mkpath "temp/bcwebsrv/general/testframework";
 mkpath "temp/bcwebsrv/winscw_udeb";
 
 my $epoc=$ENV{'EPOCROOT'} . "epoc32/";
 copy("websrv.xml",	"temp/test.xml");
 
-copy($epoc . "release/winscw/udeb/CSenServiceConnectionBCTest.dll",     "temp/bcwebsrv/winscw_udeb/CSenServiceConnectionBCTest.dll");
-copy($epoc . "winscw/c/testframework/testframework_connection.ini",     "temp/bcwebsrv/general/testframework/testframework_connection.ini");
-copy($epoc . "release/winscw/udeb/HostletConnectionBCTest.dll",         "temp/bcwebsrv/winscw_udeb/HostletConnectionBCTest.dll");
-copy($epoc . "winscw/c/testframework/TestFramework_hostlet.ini",        "temp/bcwebsrv/general/testframework/TestFramework_hostlet.ini");
-copy($epoc . "release/winscw/udeb/newsoapclassesbctest.dll",            "temp/bcwebsrv/winscw_udeb/newsoapclassesbctest.dll");
-copy($epoc . "winscw/c/testframework/testframework_newsoapclasses.ini", "temp/bcwebsrv/general/testframework/testframework_newsoapclasses.ini");
+copy($epoc . "release/winscw/udeb/CSenServiceConnectionBCTest.dll",         "temp/bcwebsrv/winscw_udeb/CSenServiceConnectionBCTest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/testframework_connection.ini",         "temp/bcwebsrv/general/testframework/testframework_connection.ini") or die "failed : $!";
+copy($epoc . "release/winscw/udeb/HostletConnectionBCTest.dll",             "temp/bcwebsrv/winscw_udeb/HostletConnectionBCTest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/TestFramework_hostlet.ini",            "temp/bcwebsrv/general/testframework/TestFramework_hostlet.ini") or die "failed : $!";
+copy($epoc . "release/winscw/udeb/newsoapclassesbctest.dll",                "temp/bcwebsrv/winscw_udeb/newsoapclassesbctest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/testframework_newsoapclasses.ini",     "temp/bcwebsrv/general/testframework/testframework_newsoapclasses.ini") or die "failed : $!";
+copy($epoc . "release/winscw/udeb/SenServDescBCTest.dll",                   "temp/bcwebsrv/winscw_udeb/SenServDescBCTest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/testframework_senservdesc.ini",        "temp/bcwebsrv/general/testframework/testframework_senservdesc.ini") or die "failed : $!";
+copy($epoc . "release/winscw/udeb/SenServiceManagerBCTest.dll",             "temp/bcwebsrv/winscw_udeb/SenServiceManagerBCTest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/testframework_senservicemanager.ini",  "temp/bcwebsrv/general/testframework/testframework_senservicemanager.ini") or die "failed : $!";
+copy($epoc . "release/winscw/udeb/SenUtilsBCTest.dll",                      "temp/bcwebsrv/winscw_udeb/SenUtilsBCTest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/testframework_senutils.ini",           "temp/bcwebsrv/general/testframework/testframework_senutils.ini") or die "failed : $!";
+copy($epoc . "release/winscw/udeb/SenFragmentBCTest.dll",                   "temp/bcwebsrv/winscw_udeb/SenFragmentBCTest.dll") or die "failed : $!";
+copy($epoc . "winscw/c/testframework/TestFramework_senfragment.ini",        "temp/bcwebsrv/general/testframework/TestFramework_senfragment.ini") or die "failed : $!";
 
 system("7z a -tzip websrv.zip ./temp/*");
